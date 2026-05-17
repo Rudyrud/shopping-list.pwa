2b88b54a4e9a1d3504791080e798878fa90f7131
Rudyrud <82507831+Rudyrud@users.noreply.github.com>
2026-05-17 17:36:24 +0100

    service-worker.js
    Update file

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-const CACHE = 'lista-compras-v1';
-const FILES = ['.', 'index.html', 'manifest.json'];
+const CACHE_NAME = 'lista-compras-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
